b8ba11b4e4f4f2c358ddc0cafccd11eca62b5c1f
AbdulrahmanMaged <abdulrahmanmaged@yahoo.com>
2023-12-17 22:32:33 +0200

    Added movement
    Added pirate ship movement and whale models and their animation functions

--- ocean.js
+++ ocean.js
@@ -8,7 +8,8 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 let container, stats;
 let camera, scene, renderer;
-let controls, water, sun, mesh;
+let controls, water, sun;
+let pirateBoatPosition ;
 
 init();
 animate();
@@ -35,6 +36,10 @@ function init() {
     camera.position.set(30, 60, 100);
     controls = new OrbitControls( camera, renderer.domElement );
     controls.target.set(0, 10, 0);
+
+    // Increase the movement speed of the controls
+    controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+    controls.dampingFactor = 0.25; // controls damping (higher means lower sensitivity)
 
     controls.maxPolarAngle = Math.PI * 0.495;
     controls.target.set( 0, 10, 0 );
@@ -119,17 +124,18 @@ function init() {
 
     //
 
-// loading boat model 
+// loading Mini boat model 
 
 const loaderMiniBoat = new GLTFLoader();
 
 loaderMiniBoat.load('model/Boat/scene.gltf', function (gltf) {
     // Set the desired position ( x ,z , y)
-    const desiredPosition = new THREE.Vector3(500, 20, 50);
+    const desiredPosition = new THREE.Vector3(0, 20, 0);
 
     // Apply the desired position to the loaded model
     gltf.scene.position.copy(desiredPosition);
-
+ // Give the Mini boat a name
+ gltf.scene.name = "MiniBoat"; 
     // Add the model to the scene
     scene.add(gltf.scene);
 }, undefined, function (error) {
@@ -141,23 +147,40 @@ const loaderPirateBoat = new GLTFLoader();
 
 loaderPirateBoat.load('model/PirateBoat/scene.gltf', function (gltf) {
     // Set the desired position ( x ,z , y)
-    const desiredPosition = new THREE.Vector3(500, 20, 500);
-
-//scale
-const desiredScale = 200;
-    // Apply the desired position to the loaded model
-    gltf.scene.position.copy(desiredPosition);
-
-//Apply scale 
+    pirateBoatPosition = new THREE.Vector3(1500, 20, 500); // Initial position
+    gltf.scene.position.copy(pirateBoatPosition);
+// Give the Pirate boat a name
+    gltf.scene.name = "PirateBoat"; 
+// Adjust the scaling factor as needed
+    const desiredScale = 200; 
     gltf.scene.scale.set(desiredScale, desiredScale, desiredScale);
 
+    // Add the model to the scene
+    scene.add(gltf.scene);
+    // Save the loaded object
+    pirateBoat = gltf.scene;
+}, undefined, function (error) {
+    console.error(error);
+});
+//////////////////////////////////////////
+
+//Loading whale 1 Model 
+const loaderWhale1 = new GLTFLoader();
+
+loaderWhale1.load('model/Whale1/scene.gltf', function (gltf) {
+    // Set the desired position ( x ,z , y)
+    const desiredPosition = new THREE.Vector3(400, -60, 150);
+
+    // Apply the desired position to the loaded model
+    gltf.scene.position.copy(desiredPosition);
+ // Give the Mini boat a name
+ gltf.scene.name = "Whale1"; 
     // Add the model to the scene
     scene.add(gltf.scene);
 }, undefined, function (error) {
     console.error(error);
 });
-
-
+///////////////////////////
     //
 
 
@@ -199,10 +222,66 @@ function onWindowResize() {
 
 function animate() {
     requestAnimationFrame(animate);
+    // Update controls and render the scene
+    PirateBoatMovement();
+    WhaleMovement();
     controls.update();
     render();
     stats.update();
 }
+
+function WhaleMovement() {
+    // Find the Whale1 object in the scene
+    const whale1 = scene.getObjectByName("Whale1");
+
+    if (whale1) {
+        // Adjust the movement ranges and speeds as needed
+        const horizontalMovementRange = 800; // How far the whale should move horizontally
+        const verticalMovementRange = -50; // How far the whale should move up and down
+        const horizontalMovementSpeed = 0.00015; // Adjust the speed of horizontal movement
+        const verticalMovementSpeed = 0.001; // Adjust the speed of vertical movement
+
+        // Calculate the horizontal movement based on sine function
+        const horizontalMovement = horizontalMovementRange * Math.sin(performance.now() * horizontalMovementSpeed);
+
+        // Calculate the vertical movement based on sine function
+        const verticalMovement = verticalMovementRange * Math.sin(performance.now() * verticalMovementSpeed);
+
+        // Set the new position for the Whale1 object
+        whale1.position.set(500, verticalMovement, horizontalMovement);
+    } else {
+        console.error("Whale1 not found in the scene or not loaded yet.");
+    }
+}
+
+
+
+function PirateBoatMovement() {
+    // Animate Pirate boat's position in a circle around MiniBoat
+    const speed = 0.0001; // Adjust the speed of movement
+    const radius = 1500; // Adjust the radius of the circle
+
+    // Get the position you want to circle about
+    const CircleAroundPosition = scene.getObjectByName("MiniBoat").position;
+
+    // Calculate new position in a circular path around MiniBoat
+    const angle = -speed * performance.now();
+    const newX = CircleAroundPosition.x + radius * Math.cos(angle);
+    const newZ = CircleAroundPosition.z + radius * Math.sin(angle);
+
+    // Update the Pirate boat's position
+    const pirateBoat = scene.getObjectByName("PirateBoat");
+    if (pirateBoat) {
+        pirateBoat.position.set(newX, pirateBoatPosition.y, newZ);
+        // Update the rotation to face towards the center
+        const directionToCenter = new THREE.Vector3(CircleAroundPosition.x - newX, 0, CircleAroundPosition.z - newZ);
+        pirateBoat.lookAt(CircleAroundPosition);
+    } else {
+        console.error("Pirate boat not found in the scene or not loaded yet.");
+    }
+}
+
+
 
 function render() {
     const time = performance.now() * 0.001;
